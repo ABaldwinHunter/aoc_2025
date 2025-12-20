3b6a6bea11b0297f29b0dbad8d0f2133a3438ca7
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2025-12-20 14:11:16 -0500

    WIP

--- day_01/one.rb
+++ day_01/one.rb
@@ -1,8 +1,10 @@
 # part 1
 #
 #
-# INPUT_FILE = 'sample.txt'
-INPUT_FILE = 'input.txt'
+INPUT_FILE = 'sample.txt'
+# zeros count is 1066
+# passes_0_count 6216 # too low
+# INPUT_FILE = 'input.txt'
 
 class Instruction
   attr_accessor :dir, :num
@@ -31,41 +33,57 @@ end
 input = File.read(INPUT_FILE).split("\n")
 turns = input.map { |instruction| Instruction.from_str(instruction) }
 
-
-zeros_count = 0
-
+zeros_count = 0 # for landings
+passes_0_count = 0
 current_pointer = 50
 
+# count each time lands at 0
 turns.each do |turn|
   puts "current pointer #{current_pointer}"
   puts "turn: #{turn.dir} #{turn.num}"
 
-  if current_pointer == 0
-    zeros_count += 1
-  end
+  # if current_pointer == 0
+  #   zeros_count += 1
+  # end
+  #
+  # count_zero_landing = true
 
   dir = turn.dir
   num = turn.num
 
   # if dir = left, then go left, or down
   # if dir = right, then go right, or up
-  #
+
+  zero_passed_this_turn = false
+
   if dir == "L"
     new_num =  current_pointer - num
 
     while new_num < 0
       new_num += 100
+      passes_0_count += 1
+      # zero_passed_this_turn = true
     end
   else
     new_num = current_pointer + num
 
     while new_num > 99
       new_num -= 100
+
+      passes_0_count += 1
+      # zero_passed_this_turn = true
     end
   end
+
+  # if new_num == 0
+  #   passes_0_count += 1
+  # end
 
   current_pointer = new_num
 end
 
 puts "running input"
-puts "zeros count is #{zeros_count}"
+# puts "zeros count is #{zeros_count}"
+puts "passes_0_count #{passes_0_count}"
+
+7282
