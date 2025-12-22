e3c12b6e6ecd4386419f394f00ba8fdc51fa1dfe
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2025-12-22 12:04:03 -0500

    WIP WIP

--- day_01/one.rb
+++ day_01/one.rb
@@ -2,9 +2,9 @@
 #
 # INPUT_FILE = 'sample.txt'
 # zeros count is 1066
-# passes_0_count 6216 # too low
-# INPUT_FILE = 'input.txt'
-INPUT_FILE = 'fromsahar.txt'
+# passes_0_count 6216 # too low 6111 6211
+INPUT_FILE = 'input.txt'
+# INPUT_FILE = 'fromsahar.txt'
 
 class Instruction
   attr_accessor :dir, :num
@@ -33,36 +33,34 @@ end
 input = File.read(INPUT_FILE).split("\n")
 turns = input.map { |instruction| Instruction.from_str(instruction) }
 
-# zeros_count = 0 # for landings
 passes_0_count = 0
 current_pointer = 50
-zero_passed_last_turn = false
+landed_at_0_on_last_time = false
 
 # count each time lands at 0
 turns.each do |turn|
   puts "current pointer #{current_pointer}"
   puts "turn: #{turn.dir} #{turn.num}"
-
-  if current_pointer == 0 && zero_passed_last_turn == false
-    puts "incrementing passes_0_count, in landed on 0 loop"
-    passes_0_count += 1
-  end
+  zero_passed_this_turn = false
 
   dir = turn.dir
   num = turn.num
 
-  zero_passed_this_turn = false
-  first_time_looping = true
+  if num > 99
+    puts "*" * 200
+  end
 
   if dir == "L"
     new_num =  current_pointer - num
+    first_pass = true
 
     while new_num < 0
+      # test = (landed_at_0_on_last_time && (num / 100) < 1)
       new_num += 100
-      passes_0_count += 1 unless (first_time_looping && current_pointer == 0) # if it's the first time, then don't
+      passes_0_count += 1 unless landed_at_0_on_last_time && first_pass && (num <= 100)
+      first_pass = false
       puts "incrementing passes_0_count, in L loop"
       zero_passed_this_turn = true
-      first_time_looping = false
     end
   else
     new_num = current_pointer + num
@@ -70,23 +68,26 @@ turns.each do |turn|
     while new_num > 99
       new_num -= 100
 
-      passes_0_count += 1 unless (first_time_looping && current_pointer == 0) # if it's the first time, then don't
+      passes_0_count += 1
       puts "incrementing passes_0_count, in R loop"
       zero_passed_this_turn = true
-      first_time_looping = false
     end
   end
-
-  zero_passed_last_turn = zero_passed_this_turn
 
   current_pointer = new_num
   puts "new num is #{new_num}"
   puts "\n"
-  puts "\n"
+
+  if current_pointer == 0
+    passes_0_count += 1 unless zero_passed_this_turn
+    landed_at_0_on_last_time = true
+  else
+    # passes_0_count += 1 unless zero_passed_this_turn
+    landed_at_0_on_last_time = false
+  end
 end
 
 puts "running input"
-# puts "zeros count is #{zeros_count}"
 puts "passes_0_count #{passes_0_count}"
 
 # 7282
@@ -95,5 +96,8 @@ puts "passes_0_count #{passes_0_count}"
 # 6019 # no
 #
 
-# answer: 5978
-# too high 5790
+# sahar answer: 5978
+# current: 5984
+# current: 5874
+#
+# 5987
