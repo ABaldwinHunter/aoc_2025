4ca75db1f8f8ab7a06cf4b58683b24c69321b5c3
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2025-12-22 09:45:48 -0500

    Not working

--- day_01/one.rb
+++ day_01/one.rb
@@ -33,18 +33,19 @@ end
 input = File.read(INPUT_FILE).split("\n")
 turns = input.map { |instruction| Instruction.from_str(instruction) }
 
-zeros_count = 0 # for landings
+# zeros_count = 0 # for landings
 passes_0_count = 0
 current_pointer = 50
+zero_passed_last_turn = false
 
 # count each time lands at 0
 turns.each do |turn|
   puts "current pointer #{current_pointer}"
   puts "turn: #{turn.dir} #{turn.num}"
 
-  # if current_pointer == 0
-  #   zeros_count += 1
-  # end
+  if current_pointer == 0 && zero_passed_last_turn == false
+    passes_0_count += 1
+  end
   #
   # count_zero_landing = true
 
@@ -62,7 +63,7 @@ turns.each do |turn|
     while new_num < 0
       new_num += 100
       passes_0_count += 1
-      # zero_passed_this_turn = true
+      zero_passed_this_turn = true
     end
   else
     new_num = current_pointer + num
@@ -71,13 +72,15 @@ turns.each do |turn|
       new_num -= 100
 
       passes_0_count += 1
-      # zero_passed_this_turn = true
+      zero_passed_this_turn = true
     end
   end
 
-  # if new_num == 0
+  # if new_num == 0 && (zero_passed_this_turn == false)
   #   passes_0_count += 1
   # end
+  #
+  zero_passed_last_turn = zero_passed_this_turn
 
   current_pointer = new_num
 end
@@ -86,4 +89,6 @@ puts "running input"
 # puts "zeros count is #{zeros_count}"
 puts "passes_0_count #{passes_0_count}"
 
-7282
+# 7282
+# 6216 # too low
+# 6643 # no
