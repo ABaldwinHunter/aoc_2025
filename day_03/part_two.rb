81f99e2417038f5173bac96f7eef3b4a843e6b6e
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2026-01-17 22:27:50 -0500

    Update

--- day_03/part_two.rb
+++ day_03/part_two.rb
@@ -14,67 +14,31 @@ BANK_LAST_INDEX = banks.first.length - 1
 results = []
 
 banks.each do |bank|
-  # check for ones that are the smallest three to the left
+  # track the largest 12 numbers, going from left to right
   #
+  largest = []
+  rejected = {}
+  rejected_count = 0
 
-  ones_to_skip = {} # val => index
-
-  # walk through and see which should be skipped.
-  #
-  # if there are none to skip, put yourself
-  #
-  # if you are a lower number than one of the other ones to skip and
-  # adjacent, then switch
-
-  bank.each.with_index do |num, i|
-    if ones_to_skip.keys.length < 3
-      ones_to_skip[num] = i # default to skipping
+  bank.each do |num|
+    if largest.count == 0
+      largest << num
     else
-      # check if a better candidate than the others
-      # if all the others are less, then definitely not a good candidate
-      #
+      latest = largest.last
 
-      copy = ones_to_skip.dup
+      if num > latest && rejected_count < 3
+        largest.pop
 
-      puts "copy"
-      puts copy
-
-      # walk through the copy to see if any to replace, from right to left
-      # :wq
-
-      copy.each do |key_num, val_index|
-        next if key_num < num
-
-        if i - val_index == 1 # 1 adjacent - need a way to check that :
-          ones_to_skip.delete(key_num)
-
-          ones_to_skip[num] = i
-        end
+        largest << num
       end
     end
   end
 
-  # going to add the skipped, original number, and final number to results
-  # in an object
-  # so that it's easier to debug later
-  #
-
-  answer = []
-
-  bank.each.with_index do |num, i|
-    answer << num unless ones_to_skip.values.include? i
-  end
-
-  answer = answer.map(&:to_s).join("").to_i
-
-  puts "answer is"
-  puts answer
-
   obj = {}
 
   obj["original"] = bank
-  obj["skipped"] = ones_to_skip
-  obj["answer"] = answer
+  obj["selected"] = largest
+  obj["answer"] = largest.map(&:to_s).join("").to_i
 
   results << obj
 end
